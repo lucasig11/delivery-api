aceb21d4a2f8644b10ad7f42f87c7724d6ad32b1
lucas <lucasfonsecard@icloud.com>
2023-04-12 01:08:00 -0300

    feat: add prisma service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { ClientModule } from './modules/client/client.module';
+import { DeliverymanModule } from './modules/deliveryman/deliveryman.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, ClientModule, DeliverymanModule],
+  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaService } from './database/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  await app.listen(3333);
 }
 bootstrap();
